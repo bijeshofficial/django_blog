9df56982052d95a3e61cf6813e7b846160786c60
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-07-12 21:09:14 +0545

    added static and templates files for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author':'Bijesh',
+        'title':'Blog Post 1',
+        'content': 'First Post',
+        'date_posted': 'July 12, 2020'
+    },
+    {
+        'author':'Rupesh',
+        'title':'Blog Post 2',
+        'content': 'Second Post',
+        'date_posted': 'July 13, 2020'
+    },
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>This is the about page.</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+
