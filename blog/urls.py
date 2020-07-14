de8fd9cdb1219c9f0c52ef36fba5651c01f07db9
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-07-14 22:37:58 +0545

    author details created

--- blog/urls.py
+++ blog/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from blog import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog_home'),
+    path('user/<str:username>', UserPostListView.as_view(), name = 'user_posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
     path('post/new/', PostCreateView.as_view(), name = 'post_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post_update'),
